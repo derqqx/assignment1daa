6b81d98dce6ce32b612352a7db02ea05c1ab76a4
derqqx <___github>
2025-09-28 05:16:06 +0500

    feat(metrics): counters, depth tracker, CSV writer

--- src/main/java/assignment1/CsvWriter.java
+++ src/main/java/assignment1/CsvWriter.java
@@ -1,12 +1,21 @@
 package assignment1;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class CsvWriter {
     public static void write(String file, String algo, int n, Metrics m, long timeMs) {
-        try (FileWriter fw = new FileWriter(file, true)) {
-            fw.write(algo + "," + n + "," + m.cmp + "," + m.moves + "," + m.depth + "," + timeMs + "\n");
+        try {
+            File f = new File(file);
+            boolean newFile = !f.exists(); // проверка новый файл или нет
+
+            try (FileWriter fw = new FileWriter(f, true)) {
+                if (newFile) {
+                    fw.write("algorithm,n,timeMs,depth,comparisons,moves\n");
+                }
+                fw.write(algo + "," + n + "," + timeMs + "," + m.depth + "," + m.cmp + "," + m.moves + "\n");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
